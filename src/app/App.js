b50bb88c610bf085ac302963a386ddc247525ecb
Igor Canko Minotto <igorcankominotto@gmail.com>
2020-10-04 18:09:05 -0300

    layout

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import { RechartLineChart } from '../charts/LineChart';
-import './App.css';
+import Container from './Container';
 
-const App = () => <div className="App">
-  <RechartLineChart />
-</div>;
+const App = () => {
+  const source = "https://pt.wikipedia.org/wiki/Elei%C3%A7%C3%A3o_municipal_de_S%C3%A3o_Paulo_em_2020";
+
+  return <div className="App">
+    <Container>
+      <h1 className="margin-bottom">Eleições São Paulo 2020</h1>
+      <RechartLineChart />
+      <small>Fonte: <a href={source}>{source}</a></small>
+    </Container>
+  </div>;
+}
 
 export default App;
